550fc76c56abd6601301e03d5820948252ee25bd
GitJIHO <jiho99322@gmail.com>
2024-06-07 14:29:45 +0900

    feat: db에 netflix.csv 데이터 추가

--- ott_recommend/models.py
+++ ott_recommend/models.py
@@ -5,11 +5,16 @@ from django.contrib.auth.models import User
 
 #DB구조
 class Content(models.Model):
-    title = models.CharField(max_length=255)
-    genre = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
     release_date = models.DateField()
-    rating = models.FloatField()
+    rating = models.CharField(max_length=10)  # 부동 소수점이 아닌 문자열로 변경    
     description = models.TextField()
+    director = models.CharField(max_length=100, blank=True, null=True)
+    cast = models.TextField(blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    duration = models.CharField(max_length=50, blank=True, null=True)
+
 
     def __str__(self):
         return self.title
--- project_ott/settings.py
+++ project_ott/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ott_recommend.apps.OttRecommendConfig',
+    'loader',
 ]
 
 MIDDLEWARE = [
