c25aa1c6957ec1c9bd75ed877095dae8ed5494c4
ksd <kimseong124@naver.com>
2024-06-13 23:52:44 +0900

    implementation of signup

--- api/serializers.py
+++ api/serializers.py
@@ -19,3 +19,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("__all__")
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+        return user
