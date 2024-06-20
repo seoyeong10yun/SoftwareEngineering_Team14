53dc0a6df29cc9c2d21e529f99e35086f5125ebe
GitJIHO <jiho99322@gmail.com>
2024-06-20 23:25:46 +0900

    feat: html 초기 설정

--- project_ott/urls.py
+++ project_ott/urls.py
@@ -1,24 +1,13 @@
-"""
-URL configuration for project_ott project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('ott_recommend.urls')),
     path('api/', include('api.urls')),
+    path('', RedirectView.as_view(url='/login/', permanent=False)),
+    path('login/', TemplateView.as_view(template_name='myapp/login.html'), name='login'),
+    path('register/', TemplateView.as_view(template_name='myapp/registration.html'), name='register'),
+    path('home/', TemplateView.as_view(template_name='myapp/home.html'), name='home'),
+    path('content/<int:content_id>/', TemplateView.as_view(template_name='myapp/contentInfo.html'), name='content-info'),
 ]
